b305c598edf275e5c095edd5556e9154f3fecb18
Oleksandr Gonchar <oleksandr_gonchar@epam.com>
2016-03-22 17:34:16 +0300

    add container to jade file

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -36,29 +36,6 @@
 (function () {
     "use strict";
 
-    angular.module("video-catalog.aside.directive", [])
-        .directive("aside", asideDir);
-
-    asideDir.$inject = ["$state"];
-    function asideDir() {
-        return {
-            restrict: "A",
-            templateUrl: 'javascripts/aside/store.aside.html',
-            controller: Aside
-        }
-    }
-
-    Aside.$inject = ["$state"];
-    function Aside($scope) {
-        $scope.logined = 10;
-        $scope.logout = 20;
-        $scope.user = 30;
-        console.log(50);
-    }
-})();
-(function () {
-    "use strict";
-
     angular.module("video-catalog.header.directive", [])
         .directive("header", headerDir);
 
@@ -73,6 +50,29 @@
 
     Header.$inject = ["$state"];
     function Header($scope) {
+        $scope.logined = 10;
+        $scope.logout = 20;
+        $scope.user = 30;
+        console.log(50);
+    }
+})();
+(function () {
+    "use strict";
+
+    angular.module("video-catalog.aside.directive", [])
+        .directive("aside", asideDir);
+
+    asideDir.$inject = ["$state"];
+    function asideDir() {
+        return {
+            restrict: "A",
+            templateUrl: 'javascripts/aside/store.aside.html',
+            controller: Aside
+        }
+    }
+
+    Aside.$inject = ["$state"];
+    function Aside($scope) {
         $scope.logined = 10;
         $scope.logout = 20;
         $scope.user = 30;
