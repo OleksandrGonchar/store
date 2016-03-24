a5652980efef968a57eb60ccde5c80a2b84b0087
Oleksandr Gonchar <oleksandr_gonchar@epam.com>
2016-03-24 14:52:06 +0300

    Update dummy item description style

--- front-end/app/javascripts/components/store.item-list/store.item-list.directive.js
+++ front-end/app/javascripts/components/store.item-list/store.item-list.directive.js
@@ -1,5 +1,40 @@
 (function () {
     "use strict";
+    var collection = [
+        {
+            name: "first item",
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        },{
+            name: "second item",
+            description: " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
+        },{
+            name: "second third",
+            description: "Harum inventore neque repellendus!"
+        },
+        {
+            name: "first item",
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        },{
+            name: "second item",
+            description: " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
+        },{
+            name: "second third",
+            description: "Harum inventore neque repellendus!"
+        },
+        {
+            name: "first item",
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        },{
+            name: "second item",
+            description: " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
+        },{
+            name: "second third",
+            description: "Harum inventore neque repellendus!"
+        },{
+            name: "second fourth",
+            description: "Doloribus illo magnam minima?"
+        }
+    ];
 
     angular.module("store.item-list.directive", [])
         .directive("itemlist", asideDir);
@@ -14,20 +49,6 @@
 
     Aside.$inject = ["$scope"];
     function Aside($scope) {
-        $scope.collection = [
-            {
-                name: "first item",
-                description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-            },{
-                name: "second item",
-                description: " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
-            },{
-                name: "second third",
-                description: "Harum inventore neque repellendus!"
-            },{
-                name: "second fourth",
-                description: "Doloribus illo magnam minima?"
-            }
-        ]
+        $scope.collection = collection;
     }
 })();
