24c17dc016c3f79783d206be82a9d16a44ed02fb
Oleksandr Gonchar <oleksandr_gonchar@epam.com>
2016-03-24 15:17:07 +0300

    Update server front end file according to https://github.com/OleksandrGonchar/store/commit/9bd10adbac98ce0d33728ad6871f7a41c89cfd1e

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -82,11 +82,45 @@
 })();
 (function () {
     "use strict";
+    var collection = [
+        {
+            name: "first item",
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        },{
+            name: "second item",
+            description: " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
+        },{
+            name: "second third",
+            description: "Harum inventore neque repellendus!"
+        },
+        {
+            name: "first item",
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        },{
+            name: "second item",
+            description: " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
+        },{
+            name: "second third",
+            description: "Harum inventore neque repellendus!"
+        },
+        {
+            name: "first item",
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        },{
+            name: "second item",
+            description: " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
+        },{
+            name: "second third",
+            description: "Harum inventore neque repellendus!"
+        },{
+            name: "second fourth",
+            description: "Doloribus illo magnam minima?"
+        }
+    ];
 
     angular.module("store.item-list.directive", [])
         .directive("itemlist", asideDir);
 
-    asideDir.$inject = ["$state"];
     function asideDir() {
         return {
             restrict: "A",
@@ -95,8 +129,8 @@
         }
     }
 
-    Aside.$inject = ["$state"];
+    Aside.$inject = ["$scope"];
     function Aside($scope) {
-
+        $scope.collection = collection;
     }
 })();
