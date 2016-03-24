bb452922ab7703f1b13762812bbe2c42f9e57a6f
Oleksandr Gonchar <oleksandr_gonchar@epam.com>
2016-03-24 17:16:40 +0300

    add google analytics script

--- front-end/app/javascripts/components/store.item-list/store.item-list.directive.js
+++ front-end/app/javascripts/components/store.item-list/store.item-list.directive.js
@@ -2,45 +2,45 @@
     "use strict";
     var collection = [
         {
-            name: "first item",
+            name: "1 first item",
             href: "http://dummyimage.com/600x400/023/fff.jpg",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
         },{
-            name: "second item",
+            name: "2 second item",
             href: "http://dummyimage.com/600x400/023/fff.jpg",
             description: " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
         },{
-            name: "second third",
+            name: "3 second third",
             href: "http://dummyimage.com/600x400/023/fff.jpg",
             description: "Harum inventore neque repellendus!"
         },
         {
-            name: "first item",
+            name: "4 first item",
             href: "http://dummyimage.com/600x400/023/fff.jpg",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
         },{
-            name: "second item",
+            name: "5 second item",
             href: "http://dummyimage.com/600x400/023/fff.jpg",
             description: " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
         },{
-            name: "second third",
+            name: "6 second third",
             href: "http://dummyimage.com/600x400/023/fff.jpg",
             description: "Harum inventore neque repellendus!"
         },
         {
-            name: "first item",
+            name: "7 first item",
             href: "http://dummyimage.com/600x400/023/fff.jpg",
             description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
         },{
-            name: "second item",
+            name: "8 second item",
             href: "http://dummyimage.com/600x400/023/fff.jpg",
             description: " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
         },{
-            name: "second third",
+            name: "9 second third",
             href: "http://dummyimage.com/600x400/023/fff.jpg",
             description: "Harum inventore neque repellendus!"
         },{
-            name: "second fourth",
+            name: "10 second fourth",
             href: "http://dummyimage.com/600x400/023/fff.jpg",
             description: "Doloribus illo magnam minima?"
         }
