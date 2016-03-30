4d3a2c3cbf42bf8a3de7f52e5889dcb29a0fe39e
Oleksandr Gonchar <oleksandr_gonchar@epam.com>
2016-03-30 11:19:11 +0300

    add dummy response on '/item' in 'store.item-list'

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -211,6 +211,24 @@
 
     Aside.$inject = ["$scope"];
     function Aside($scope) {
+        var xhr = new XMLHttpRequest();
+
+        // 2. Конфигурируем его: GET-запрос на URL 'phones.json'
+        xhr.open('GET', '/items', false);
+
+        // 3. Отсылаем запрос
+        xhr.send();
+
+        // 4. Если код ответа сервера не 200, то это ошибка
+        if (xhr.status != 200) {
+            // обработать ошибку
+            alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+        } else {
+            // вывести результат
+            alert( xhr.responseText ); // responseText -- текст ответа.
+        }
+
+
         $scope.collection = collection;
     }
 })();
