68a83de0c8f7ace3d431173e020ba2977ab5efbd
Oleksandr Gonchar <oleksandr_gonchar@epam.com>
2016-03-30 12:37:50 +0300

    dummy item send from server to UI

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -98,6 +98,75 @@
         console.log(50);
     }
 })();
+(function () {
+    "use strict";
+    var collection /*= [
+        {
+            name: "1 first item",
+            href: "http://dummyimage.com/600x400/023/fff.jpg",
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        },{
+            name: "2 second item",
+            href: "http://dummyimage.com/600x400/023/fff.jpg",
+            description: " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
+        },{
+            name: "3 second third",
+            href: "http://dummyimage.com/600x400/023/fff.jpg",
+            description: "Harum inventore neque repellendus!"
+        },
+        {
+            name: "4 first item",
+            href: "http://dummyimage.com/600x400/023/fff.jpg",
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        },{
+            name: "5 second item",
+            href: "http://dummyimage.com/600x400/023/fff.jpg",
+            description: " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
+        },{
+            name: "6 second third",
+            href: "http://dummyimage.com/600x400/023/fff.jpg",
+            description: "Harum inventore neque repellendus!"
+        },
+        {
+            name: "7 first item",
+            href: "http://dummyimage.com/600x400/023/fff.jpg",
+            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        }/!*,{
+            name: "8 second item",
+            href: "http://dummyimage.com/600x400/023/fff.jpg",
+            description: " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
+        },{
+            name: "9 second third",
+            href: "http://dummyimage.com/600x400/023/fff.jpg",
+            description: "Harum inventore neque repellendus!"
+        },{
+            name: "10 second fourth",
+            href: "http://dummyimage.com/600x400/023/fff.jpg",
+            description: "Doloribus illo magnam minima?"
+        }*!/
+    ];*/
+
+    angular.module("store.item-list.directive", [])
+        .directive("itemlist", asideDir);
+
+    function asideDir() {
+        return {
+            restrict: "A",
+            templateUrl: 'javascripts/components/store.item-list/store.item-list.html',
+            controller: Aside
+        }
+    }
+
+    Aside.$inject = ["$scope", "$http"];
+    function Aside($scope, $http) {
+        $http.get('item').success(function(data) {
+            $scope.collection = data;
+            console.log(data);
+        });
+
+        $scope.collection = collection;
+    }
+})();
 // Back to top Directive
 (function () {
     "use strict";
@@ -147,89 +216,5 @@
             template: svgIcon(),
             controller: "ScrollController"
         }
-    }
-})();
-
-(function () {
-    "use strict";
-    var collection = [
-        {
-            name: "1 first item",
-            href: "http://dummyimage.com/600x400/023/fff.jpg",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-        },{
-            name: "2 second item",
-            href: "http://dummyimage.com/600x400/023/fff.jpg",
-            description: " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
-        },{
-            name: "3 second third",
-            href: "http://dummyimage.com/600x400/023/fff.jpg",
-            description: "Harum inventore neque repellendus!"
-        },
-        {
-            name: "4 first item",
-            href: "http://dummyimage.com/600x400/023/fff.jpg",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-        },{
-            name: "5 second item",
-            href: "http://dummyimage.com/600x400/023/fff.jpg",
-            description: " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
-        },{
-            name: "6 second third",
-            href: "http://dummyimage.com/600x400/023/fff.jpg",
-            description: "Harum inventore neque repellendus!"
-        },
-        {
-            name: "7 first item",
-            href: "http://dummyimage.com/600x400/023/fff.jpg",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-        }/*,{
-            name: "8 second item",
-            href: "http://dummyimage.com/600x400/023/fff.jpg",
-            description: " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
-        },{
-            name: "9 second third",
-            href: "http://dummyimage.com/600x400/023/fff.jpg",
-            description: "Harum inventore neque repellendus!"
-        },{
-            name: "10 second fourth",
-            href: "http://dummyimage.com/600x400/023/fff.jpg",
-            description: "Doloribus illo magnam minima?"
-        }*/
-    ];
-
-    angular.module("store.item-list.directive", [])
-        .directive("itemlist", asideDir);
-
-    function asideDir() {
-        return {
-            restrict: "A",
-            templateUrl: 'javascripts/components/store.item-list/store.item-list.html',
-            controller: Aside
-        }
-    }
-
-    Aside.$inject = ["$scope"];
-    function Aside($scope) {
-        var xhr = new XMLHttpRequest();
-
-        // 2. Конфигурируем его: GET-запрос на URL 'phones.json'
-        xhr.open('GET', 'item', false);
-
-        // 3. Отсылаем запрос
-        xhr.send();
-
-        // 4. Если код ответа сервера не 200, то это ошибка
-        if (xhr.status != 200) {
-            // обработать ошибку
-            console.log( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
-            console.log(xhr.response);
-        } else {
-            // вывести результат
-            console.log( xhr.responseText ); // responseText -- текст ответа.
-        }
-
-
-        $scope.collection = collection;
     }
 })();
--- routes/item.collection.js
+++ routes/item.collection.js
@@ -4,53 +4,54 @@ var router = express.Router();
 
 var collection = [
     {
-        name: "1 first item",
-        href: "http://dummyimage.com/600x400/023/fff.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        "name": "1 first item",
+        "href": "http://dummyimage.com/600x400/023/f0f.jpg",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
     },{
-        name: "2 second item",
-        href: "http://dummyimage.com/600x400/023/fff.jpg",
-        description: " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
+        "name": "2 second item",
+        "href": "http://dummyimage.com/600x400/023/f0f.jpg",
+        "description": " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
     },{
-        name: "3 second third",
-        href: "http://dummyimage.com/600x400/023/fff.jpg",
-        description: "Harum inventore neque repellendus!"
+        "name": "3 second third",
+        "href": "http://dummyimage.com/600x400/023/f0f.jpg",
+        "description": "Harum inventore neque repellendus!"
     },
     {
-        name: "4 first item",
-        href: "http://dummyimage.com/600x400/023/fff.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        "name": "4 first item",
+        "href": "http://dummyimage.com/600x400/023/f0f.jpg",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
     },{
-        name: "5 second item",
-        href: "http://dummyimage.com/600x400/023/fff.jpg",
-        description: " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
+        "name": "5 second item",
+        "href": "http://dummyimage.com/600x400/023/f0f.jpg",
+        "description": " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
     },{
-        name: "6 second third",
-        href: "http://dummyimage.com/600x400/023/fff.jpg",
-        description: "Harum inventore neque repellendus!"
+        "name": "6 second third",
+        "href": "http://dummyimage.com/600x400/023/f0f.jpg",
+        "description": "Harum inventore neque repellendus!"
     },
     {
-        name: "7 first item",
-        href: "http://dummyimage.com/600x400/023/fff.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+        "name": "7 first item",
+        "href": "http://dummyimage.com/600x400/023/f0f.jpg",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
     },{
-     name: "8 second item",
-     href: "http://dummyimage.com/600x400/023/fff.jpg",
-     description: " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
+     "name": "8 second item",
+     "href": "http://dummyimage.com/600x400/023/f0f.jpg",
+     "description": " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
      },{
-     name: "9 second third",
-     href: "http://dummyimage.com/600x400/023/fff.jpg",
-     description: "Harum inventore neque repellendus!"
+     "name": "9 second third",
+     "href": "http://dummyimage.com/600x400/023/f0f.jpg",
+     "description": "Harum inventore neque repellendus!"
      },{
-     name: "10 second fourth",
-     href: "http://dummyimage.com/600x400/023/fff.jpg",
-     description: "Doloribus illo magnam minima?"
+     "name": "10 second fourth",
+     "href": "http://dummyimage.com/600x400/023/f0f.jpg",
+     "description": "Doloribus illo magnam minima?"
      }
 ];
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     console.log('\n item here is \n' + req);
+    req.is('application/json');
     res.send(collection);
 });
 
