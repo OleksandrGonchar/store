e42c1deae814e3561fb37967e0e6d8b4bc2556f6
Oleksandr Gonchar <oleksandr_gonchar@epam.com>
2016-03-30 11:49:32 +0300

    finally dummy router add item list

--- front-end/app/javascripts/components/store.item-list/store.item-list.directive.js
+++ front-end/app/javascripts/components/store.item-list/store.item-list.directive.js
@@ -62,7 +62,7 @@
         var xhr = new XMLHttpRequest();
 
         // 2. Конфигурируем его: GET-запрос на URL 'phones.json'
-        xhr.open('GET', '/items', false);
+        xhr.open('GET', 'item', false);
 
         // 3. Отсылаем запрос
         xhr.send();
@@ -70,11 +70,11 @@
         // 4. Если код ответа сервера не 200, то это ошибка
         if (xhr.status != 200) {
             // обработать ошибку
-            alert( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
+            console.log( xhr.status + ': ' + xhr.statusText ); // пример вывода: 404: Not Found
             console.log(xhr.response);
         } else {
             // вывести результат
-            alert( xhr.responseText ); // responseText -- текст ответа.
+            console.log( xhr.responseText ); // responseText -- текст ответа.
         }
 
 
--- routes/item.collection.js
+++ routes/item.collection.js
@@ -1,9 +1,57 @@
 var express = require('express');
 var router = express.Router();
 
+
+var collection = [
+    {
+        name: "1 first item",
+        href: "http://dummyimage.com/600x400/023/fff.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    },{
+        name: "2 second item",
+        href: "http://dummyimage.com/600x400/023/fff.jpg",
+        description: " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
+    },{
+        name: "3 second third",
+        href: "http://dummyimage.com/600x400/023/fff.jpg",
+        description: "Harum inventore neque repellendus!"
+    },
+    {
+        name: "4 first item",
+        href: "http://dummyimage.com/600x400/023/fff.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    },{
+        name: "5 second item",
+        href: "http://dummyimage.com/600x400/023/fff.jpg",
+        description: " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
+    },{
+        name: "6 second third",
+        href: "http://dummyimage.com/600x400/023/fff.jpg",
+        description: "Harum inventore neque repellendus!"
+    },
+    {
+        name: "7 first item",
+        href: "http://dummyimage.com/600x400/023/fff.jpg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    },{
+     name: "8 second item",
+     href: "http://dummyimage.com/600x400/023/fff.jpg",
+     description: " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
+     },{
+     name: "9 second third",
+     href: "http://dummyimage.com/600x400/023/fff.jpg",
+     description: "Harum inventore neque repellendus!"
+     },{
+     name: "10 second fourth",
+     href: "http://dummyimage.com/600x400/023/fff.jpg",
+     description: "Doloribus illo magnam minima?"
+     }
+];
+
 /* GET users listing. */
-router.get('/item', function(req, res, next) {
-    res.send('you need item');
+router.get('/', function(req, res, next) {
+    console.log('\n item here is \n' + req);
+    res.send(collection);
 });
 
 module.exports = router;
