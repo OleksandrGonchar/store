99d1f6669ca141351fb77b4dc43019f18dd6c908
Oleksandr Gonchar <oleksandr_gonchar@epam.com>
2016-04-15 13:25:32 +0300

    change protocol for image to "https"

--- routes/item.collection.js
+++ routes/item.collection.js
@@ -5,45 +5,45 @@ var router = express.Router();
 var collection = [
     {
         "name": "1 first item",
-        "href": "http://dummyimage.com/600x400/023/f0f.jpg",
+        "href": "https://dummyimage.com/600x400/023/f0f.jpg",
         "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
     }, {
         "name": "2 second item",
-        "href": "http://dummyimage.com/600x400/023/f0f.jpg",
+        "href": "https://dummyimage.com/600x400/023/f0f.jpg",
         "description": " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
     }, {
         "name": "3 second third",
-        "href": "http://dummyimage.com/600x400/023/f0f.jpg",
+        "href": "https://dummyimage.com/600x400/023/f0f.jpg",
         "description": "Harum inventore neque repellendus!"
     },
     {
         "name": "4 first item",
-        "href": "http://dummyimage.com/600x400/023/f0f.jpg",
+        "href": "https://dummyimage.com/600x400/023/f0f.jpg",
         "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
     }, {
         "name": "5 second item",
-        "href": "http://dummyimage.com/600x400/023/f0f.jpg",
+        "href": "https://dummyimage.com/600x400/023/f0f.jpg",
         "description": " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
     }, {
         "name": "6 second third",
-        "href": "http://dummyimage.com/600x400/023/f0f.jpg",
+        "href": "https://dummyimage.com/600x400/023/f0f.jpg",
         "description": "Harum inventore neque repellendus!"
     },
     {
         "name": "7 first item",
-        "href": "http://dummyimage.com/600x400/023/f0f.jpg",
+        "href": "https://dummyimage.com/600x400/023/f0f.jpg",
         "description": "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
     }, {
         "name": "8 second item",
-        "href": "http://dummyimage.com/600x400/023/f0f.jpg",
+        "href": "https://dummyimage.com/600x400/023/f0f.jpg",
         "description": " Aspernatur corporis, cum delectus deleniti, ex iure laboriosam libero, molestiae mollitia sequi suscipit temporibus!"
     }, {
         "name": "9 second third",
-        "href": "http://dummyimage.com/600x400/023/f0f.jpg",
+        "href": "https://dummyimage.com/600x400/023/f0f.jpg",
         "description": "Harum inventore neque repellendus!"
     }, {
         "name": "10 second fourth",
-        "href": "http://dummyimage.com/600x400/023/f0f.jpg",
+        "href": "https://dummyimage.com/600x400/023/f0f.jpg",
         "description": "Doloribus illo magnam minima?"
     }
 ];
