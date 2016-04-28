7cea2033a5b7c6a83171a34855d6ba9a17bf882a
Oleksandr Gonchar <oleksandr_gonchar@epam.com>
2016-04-28 16:28:46 +0300

    add dummy database to user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,49 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+
+/*DB part*/
+mongoose.connect('mongodb://localhost/cdp');
+
+var shema = mongoose.Schema({
+  "eser": String,
+  "id": Number
+});
+
+var User = mongoose.model("user", shema);
+var jhon = new User({
+  "eser": "not user",
+  "id": 0
+});
+jhon.save(function (err, model) {
+  if (err) console.log(err);
+  User.find(function (err, users) {
+    if (err) console.log(err);
+    else console.log(users);
+  })
+});
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:id', function(req, res, next) {
+  User.findOne({id : req.params.id},function (err, users) {
+    if (err) console.log(err);
+    else {
+      res.type('json');
+      res.send(users);
+    }
+  })
 });
+router.get('/', function(req, res, errorLogger) {
+  User.find(function (err, users) {
+    if (err) console.log(err);
+    else {
+      res.type('json');
+      res.send(users);
+    }
+  })
+});
+function errorLogger(e){
+  console.log(e);
+}
 
 module.exports = router;
