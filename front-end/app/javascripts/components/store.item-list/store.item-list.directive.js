6e476a6dfa7b195c89779306c97ab314c13edc9b
Oleksandr Gonchar <oleksandr_gonchar@epam.com>
2016-05-17 13:55:15 +0300

    restore build

--- front-end/app/javascripts/components/store.item-list/store.item-list.directive.js
+++ front-end/app/javascripts/components/store.item-list/store.item-list.directive.js
@@ -4,14 +4,6 @@
     angular.module("store.item-list.directive", [])
         .controller("itemlist", itemlist);
 
-    function asideDir() {
-        return {
-            //restrict: "A",
-            //templateUrl: "javascripts/components/store.item-list/store.item-list.html",
-            //controller: Aside
-        }
-    }
-
     itemlist.$inject = ["$element", "$scope", "$http"];
     function itemlist( $element, $scope, $http) {
         console.log($element);
@@ -19,12 +11,15 @@
             method: 'GET',
             url: 'goods'
         }).then(function successCallback(response) {
-            $scope.collection = response.data;
-            console.log(response.data);
+            $scope.fullCollection = response.data;
+            $scope.collection = $scope.fullCollection;
+            console.log(response.data, " \n", $scope.fullCollection);
             $scope.alreadyLoad = "already-load";
-            var a = document.getElementsByClassName("item-image");
-            //a.bind('load', alert(33))
-            console.log(a);
+
+            $scope.showMore = function(fullCollection) {
+
+            };
+
         }, function errorCallback(response) {
             console.log(response);
         });
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -167,38 +167,6 @@
 (function () {
     "use strict";
 
-    angular.module("store.item-list.directive", [])
-        .controller("itemlist", itemlist);
-
-    function asideDir() {
-        return {
-            //restrict: "A",
-            //templateUrl: "javascripts/components/store.item-list/store.item-list.html",
-            //controller: Aside
-        }
-    }
-
-    itemlist.$inject = ["$element", "$scope", "$http"];
-    function itemlist( $element, $scope, $http) {
-        console.log($element);
-        $http({
-            method: 'GET',
-            url: 'goods'
-        }).then(function successCallback(response) {
-            $scope.collection = response.data;
-            console.log(response.data);
-            $scope.alreadyLoad = "already-load";
-            var a = document.getElementsByClassName("item-image");
-            //a.bind('load', alert(33))
-            console.log(a);
-        }, function errorCallback(response) {
-            console.log(response);
-        });
-    }
-})();
-(function () {
-    "use strict";
-
     angular.module("store.pre-loader.directive", [])
         .directive("preLoader", preLoad);
 
@@ -238,5 +206,32 @@
             console.log(response);
         });
         console.log($element);
+    }
+})();
+(function () {
+    "use strict";
+
+    angular.module("store.item-list.directive", [])
+        .controller("itemlist", itemlist);
+
+    itemlist.$inject = ["$element", "$scope", "$http"];
+    function itemlist( $element, $scope, $http) {
+        console.log($element);
+        $http({
+            method: 'GET',
+            url: 'goods'
+        }).then(function successCallback(response) {
+            $scope.fullCollection = response.data;
+            $scope.collection = $scope.fullCollection;
+            console.log(response.data, " \n", $scope.fullCollection);
+            $scope.alreadyLoad = "already-load";
+
+            $scope.showMore = function(fullCollection) {
+
+            };
+
+        }, function errorCallback(response) {
+            console.log(response);
+        });
     }
 })();
